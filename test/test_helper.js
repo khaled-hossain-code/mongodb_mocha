9211d5bec9d5b0c73eb77eee54a7fc0dfc59cc0f
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-01-06 05:44:21 +0600

    drop db before each test

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,4 +4,12 @@ mongoose.connect('mongodb://localhost/users_test');
 
 mongoose.connection
   .once('open', () => console.log('good to go'))
-  .on('error', (err) => console.error(err))
+  .on('error', (err) => console.error(err));
+
+beforeEach(async () => {
+  try {
+    await mongoose.connection.collections.users.drop();
+  }catch (err) {
+    console.error(err);
+  }
+})
