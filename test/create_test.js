e46b043d0d038690f3cfe21541c425cb68a0006f
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-01-06 05:59:01 +0600

    Before all test please connect to mongodb

--- test/create_test.js
+++ test/create_test.js
@@ -2,11 +2,13 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', async () => {
     const joe = new User({
       name: 'joe'
     });
 
-    joe.save();
+    await joe.save();
+    assert(joe.isNew === false);
   })
-})
+});
+
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+// Run only one time before all test
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true
+  });
 
-mongoose.connection
-  .once('open', () => console.log('good to go'))
-  .on('error', (err) => console.error(err));
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (err) => console.error(err));
 
+})
+
+// Run before each test
 beforeEach(async () => {
   try {
     await mongoose.connection.collections.users.drop();
-  }catch (err) {
+  } catch (err) {
     console.error(err);
   }
 })
